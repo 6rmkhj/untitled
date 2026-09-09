830040372c02ce441f14bab9cfe869a0fef7ce30
6rmkhj <6rmkhj@yu.ac.kr>
2026-09-09 13:48:15 +0900

    chore: advance scene schema for monster objects

--- Assets/Scripts/SignalHaulSceneMarker.cs
+++ Assets/Scripts/SignalHaulSceneMarker.cs
@@ -4,7 +4,7 @@ namespace SignalHaul
 {
     public sealed class SignalHaulSceneMarker : MonoBehaviour
     {
-        public const int CurrentVersion = 6;
+        public const int CurrentVersion = 7;
 
         [SerializeField, HideInInspector] private int sceneVersion = CurrentVersion;
         public int SceneVersion => sceneVersion;
